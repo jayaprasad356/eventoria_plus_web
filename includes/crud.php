a59bcb4ae1252bb8349b6c75793e32960539b445
jayaprasad356 <jayaprasad356@gmail.com>
2022-07-11 14:55:51 +0530

    crud changed

--- includes/crud.php
+++ includes/crud.php
@@ -19,9 +19,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "eventoria_plus";    // Change as required
+    private $db_user = "u743445510_ep";  // Change as required
+    private $db_pass = "Event@2022";  // Change as required
+    private $db_name = "u743445510_ep";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
