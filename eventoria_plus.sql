d2e95137f8fce76ecc2b9a5cb7ad5f28bccb8303
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-03 11:11:08 +0530

    db

--- eventoria_plus.sql
+++ eventoria_plus.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 15, 2022 at 01:21 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 03, 2022 at 05:40 AM
+-- Server version: 10.5.13-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `eventoria_plus`
+-- Database: `u743445510_ep`
 --
 
 -- --------------------------------------------------------
@@ -41,7 +41,9 @@ CREATE TABLE `address` (
 --
 
 INSERT INTO `address` (`id`, `name`, `address`, `district`, `pincode`, `state`) VALUES
-(1, 'prasad', 'street', 'sholapuram', '612828', 'kumbakonam');
+(1, 'prasad', 'street', 'sholapuram', '612828', 'kumbakonam'),
+(2, 'c', 'vv', 'bbv', 'ff', 'gv'),
+(3, 'Ad', 'udbxb', '134556', '18$849', 'india');
 
 -- --------------------------------------------------------
 
@@ -75,6 +77,8 @@ INSERT INTO `categories` (`id`, `name`, `image`, `status`) VALUES
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
+  `order_date` varchar(200) DEFAULT NULL,
+  `event_date` varchar(100) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `address_id` int(11) DEFAULT NULL,
   `venue_id` int(11) DEFAULT NULL,
@@ -83,7 +87,6 @@ CREATE TABLE `orders` (
   `type` text DEFAULT NULL,
   `start_time` text DEFAULT NULL,
   `end_time` text DEFAULT NULL,
-  `prices` text DEFAULT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -91,9 +94,54 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `address_id`, `venue_id`, `package_id`, `price`, `type`, `start_time`, `end_time`, `prices`, `status`) VALUES
-(1, 1, 1, NULL, 1, '3000', 'own', '', '', '', 1),
-(5, 1, NULL, 3, 1, '800', 'venue', '20:00', '20:00', '3000', 1);
+INSERT INTO `orders` (`id`, `order_date`, `event_date`, `user_id`, `address_id`, `venue_id`, `package_id`, `price`, `type`, `start_time`, `end_time`, `status`) VALUES
+(1, NULL, NULL, 1, 1, NULL, 1, '3000', 'own', '', '', 1),
+(5, NULL, NULL, 1, NULL, 3, 1, '800', 'venue', '20:00', '20:00', 1),
+(7, NULL, NULL, 1, NULL, 3, 1, '3000', 'venue', '20:00', '20:00', 1),
+(8, NULL, NULL, 1, NULL, 3, 1, '', 'venue', NULL, NULL, 1),
+(9, NULL, NULL, 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(10, '2022', NULL, 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(11, '2022-08-02', NULL, 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(12, '2022-08-02', NULL, 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(13, '2022-08-02', NULL, 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(14, '2022-08-02', '2022-08-05', 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(15, '2022-08-02', '2022-08-07', 1, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(16, '2022-08-02', '2022-08-07', 1, NULL, 3, 1, '3400', 'venue', NULL, NULL, 1),
+(17, '2022-08-03', '2022-08-03', 2, NULL, 3, 1, '6000', 'venue', NULL, NULL, 1),
+(18, '2022-08-03', '2022-08-07', 3, NULL, 3, 1, '3200', 'venue', NULL, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_timeslot`
+--
+
+CREATE TABLE `orders_timeslot` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `venue_id` int(11) DEFAULT NULL,
+  `time_slot_id` int(11) DEFAULT NULL,
+  `start_time` varchar(100) DEFAULT NULL,
+  `end_time` varchar(100) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders_timeslot`
+--
+
+INSERT INTO `orders_timeslot` (`id`, `user_id`, `order_id`, `venue_id`, `time_slot_id`, `start_time`, `end_time`, `price`) VALUES
+(1, 1, 8, 3, 1, '20:00', '20:00', 3000),
+(2, 1, 9, 3, 1, '20:00', '20:00', 3000),
+(3, 1, 10, 3, 1, '20:00', '20:00', 3000),
+(4, 1, 11, 3, 1, '20:00', '20:00', 3000),
+(5, 1, 12, 3, 1, '20:00', '20:00', 3000),
+(6, 1, 14, 3, 1, '20:00', '20:00', 3000),
+(7, 1, 15, 3, 1, '20:00', '20:00', 3000),
+(8, 1, 16, 3, 4, '11:45', '15:40', 400),
+(9, 2, 17, 3, 1, '20:00', '20:00', 3000),
+(10, 3, 18, 3, 3, '16:19', '20:24', 200);
 
 -- --------------------------------------------------------
 
@@ -122,7 +170,7 @@ CREATE TABLE `packages` (
 --
 
 INSERT INTO `packages` (`id`, `name`, `cover_photo`, `image1`, `image2`, `image3`, `image4`, `recommend`, `price`, `category_id`, `description`, `pincode`, `status`) VALUES
-(1, 'Basic Birthday', 'upload/images/1657836058.6088.png', 'upload/images/1657836058.6638.png', 'upload/images/1657836058.7648.png', 'upload/images/1657836058.9099.png', 'upload/images/1657836059.2159.png', 1, '3000', 1, 'Basic Plan for Afforadable Price', '612345', 1);
+(1, 'Basic Birthday', 'upload/images/1657836058.6088.png', 'upload/images/1657836058.6638.png', 'upload/images/1657836058.7648.png', NULL, 'upload/images/1657836059.2159.png', 1, '3000', 1, 'Basic Plan for Afforadable Price', '612345', 1);
 
 -- --------------------------------------------------------
 
@@ -166,7 +214,9 @@ CREATE TABLE `timeslots` (
 
 INSERT INTO `timeslots` (`id`, `venue_id`, `start_time`, `end_time`, `prices`) VALUES
 (1, 3, '20:00', '20:00', 3000),
-(2, 4, '03:45', '16:00', 500);
+(2, 4, '03:45', '16:00', 500),
+(3, 3, '16:19', '20:24', 200),
+(4, 3, '11:45', '15:40', 400);
 
 -- --------------------------------------------------------
 
@@ -186,7 +236,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `mobile`, `pincode`) VALUES
-(1, 'Prasad', '8778624681', '612345');
+(1, 'Prasad', '8778624681', '612345'),
+(2, 'S.k.yadav', '8827241086', '497001'),
+(3, 'tamil', '6382088746', '612345');
 
 -- --------------------------------------------------------
 
@@ -236,6 +288,12 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders_timeslot`
+--
+ALTER TABLE `orders_timeslot`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `packages`
 --
 ALTER TABLE `packages`
@@ -273,7 +331,7 @@ ALTER TABLE `venues`
 -- AUTO_INCREMENT for table `address`
 --
 ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -285,7 +343,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `orders_timeslot`
+--
+ALTER TABLE `orders_timeslot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `packages`
@@ -303,13 +367,13 @@ ALTER TABLE `slides`
 -- AUTO_INCREMENT for table `timeslots`
 --
 ALTER TABLE `timeslots`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `venues`
