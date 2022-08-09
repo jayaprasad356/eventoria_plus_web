2cb2198e400ee6950c6dd6ec2ac0bd2ff0607287
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-09 23:25:13 +0530

    orders list

--- api/orders-list.php
+++ api/orders-list.php
@@ -32,7 +32,7 @@ $num = $db->numRows($res);
 if ($num >= 1){
     if($type=='own'){
     
-        $sql = "SELECT *,orders.id,orders.price AS id,packages.name AS package_name  FROM orders,packages,address WHERE orders.package_id = packages.id AND orders.address_id = address.id AND orders.user_id='$user_id' ORDER BY orders.id DESC";
+        $sql = "SELECT *,orders.id  AS id,orders.price AS price,packages.name AS package_name  FROM orders,packages,address WHERE orders.package_id = packages.id AND orders.address_id = address.id AND orders.user_id='$user_id' ORDER BY orders.id DESC";
         $db->sql($sql);
         $res = $db->getResult();
         $num = $db->numRows($res);
@@ -56,7 +56,7 @@ if ($num >= 1){
             print_r(json_encode($response));
         }
     else{
-        $sql = "SELECT *,orders.id,orders.price AS id,packages.name AS package_name FROM orders,packages,venues WHERE orders.package_id = packages.id AND orders.venue_id = venues.id AND orders.user_id='$user_id' ORDER BY orders.id DESC";
+        $sql = "SELECT *,orders.id  AS id,orders.price AS price,packages.name AS package_name FROM orders,packages,venues WHERE orders.package_id = packages.id AND orders.venue_id = venues.id AND orders.user_id='$user_id' ORDER BY orders.id DESC";
         $db->sql($sql);
         $res = $db->getResult();
         $num = $db->numRows($res);
